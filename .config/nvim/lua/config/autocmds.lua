88df68d276e0cce3617314d2fd7fdca3bbb0adce
henrythomaz <henrythcampos@gmail.com>
2026-01-02 08:28:29 -0300

    feat(nvim): Update autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -5,7 +5,6 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 
 -- Disable the concealing in some file formats
--- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "json", "jsonc", "markdown" },
 	callback = function()
