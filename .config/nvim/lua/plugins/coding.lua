579fd7a99ddc22d5d0e63e82a3219a21066e9a6d
henrythomaz <henrythcampos@gmail.com>
2026-01-02 08:31:09 -0300

    feat(nvim): Update coding.lua

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -22,14 +22,14 @@ return {
 		end,
 	},
 
-	-- Better increase/descrease
+	-- Better increase/decrease
 	{
 		"monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-    },
+		-- stylua: ignore
+		keys = {
+			{ "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+			{ "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+		},
 		config = function()
 			local augend = require("dial.augend")
 			require("dial.config").augends:register_group({
@@ -45,7 +45,7 @@ return {
 		end,
 	},
 
-	-- copilot
+	-- Copilot (desabilitado)
 	{
 		"zbirenbaum/copilot.lua",
 		enabled = false,
