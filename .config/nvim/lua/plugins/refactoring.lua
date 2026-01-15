df57ea89e5cba073dcf218983aa193622b6f0b1d
henrythomaz <henrythcampos@gmail.com>
2026-01-15 10:21:10 -0300

    feat: Update refactoring.lua

--- .config/nvim/lua/plugins/refactoring.lua
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -1,5 +1,7 @@
 return {
-	-- Incremental rename
+	-- ══════════════════════════════════════════════════════════
+	-- Incremental Rename (já temos no coding.lua)
+	-- ══════════════════════════════════════════════════════════
 	{
 		"smjonas/inc-rename.nvim",
 		cmd = "IncRename",
@@ -10,18 +12,62 @@ return {
 					return ":IncRename " .. vim.fn.expand("<cword>")
 				end,
 				desc = "Incremental rename",
-				mode = "n",
-				noremap = true,
 				expr = true,
 			},
 		},
 		config = true,
 	},
 
-	-- Refactoring tool
+	-- ══════════════════════════════════════════════════════════
+	-- Refactoring.nvim - Refatorações automáticas
+	-- ══════════════════════════════════════════════════════════
 	{
 		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
 		keys = {
+			{
+				"<leader>re",
+				function()
+					require("refactoring").refactor("Extract Function")
+				end,
+				mode = "x",
+				desc = "Extract Function",
+			},
+			{
+				"<leader>rf",
+				function()
+					require("refactoring").refactor("Extract Function To File")
+				end,
+				mode = "x",
+				desc = "Extract Function To File",
+			},
+			{
+				"<leader>rv",
+				function()
+					require("refactoring").refactor("Extract Variable")
+				end,
+				mode = "x",
+				desc = "Extract Variable",
+			},
+			{
+				"<leader>ri",
+				function()
+					require("refactoring").refactor("Inline Variable")
+				end,
+				mode = { "n", "x" },
+				desc = "Inline Variable",
+			},
+			{
+				"<leader>rb",
+				function()
+					require("refactoring").refactor("Extract Block")
+				end,
+				mode = "n",
+				desc = "Extract Block",
+			},
 			{
 				"<leader>r",
 				function()
@@ -30,9 +76,7 @@ return {
 					})
 				end,
 				mode = "v",
-				noremap = true,
-				silent = true,
-				expr = false,
+				desc = "Refactoring menu",
 			},
 		},
 		opts = {},
